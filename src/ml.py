b410726b695c27fa3bf3cc1ca346d7b4930b2b0f
Matias-07 <matias.ferrari.m@gmail.com>
2026-08-29 23:14:32 -0300

    Generating the data

--- src/ml.py
+++ src/ml.py
@@ -1,2 +1,27 @@
 import numpy as np
+import matplotlib.pyplot as plt 
 
+N_PUNTOS = 100
+DESVIO_RUIDO = 1.5
+SEMILLA = 42
+
+if __name__ == "__main__":
+    #creamos el array inicial de valores para X
+    array_x = np.linspace(0, 10, N_PUNTOS)
+
+    #creamos el ruido
+    generador = np.random.default_rng(SEMILLA)
+    array_ruido = generador.normal(0,DESVIO_RUIDO,N_PUNTOS)
+
+    valores_reales_y = 3*array_x + 2
+
+    valores_devio_y = valores_reales_y + array_ruido
+
+    #plot - muestra los datos
+    fig, ax = plt.subplots(figsize=(20, 6))
+
+    ax.plot(array_x, valores_reales_y, 'o') 
+    ax.plot(array_x, valores_devio_y, 'x')
+    
+    plt.show()
+    
