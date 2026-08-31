70e8fa38f45f782e1d27c97c178a36931175df68
Matias-07 <matias.ferrari.m@gmail.com>
2026-08-31 19:13:06 -0300

    funciones de prediccion y perdida

--- src/ml.py
+++ src/ml.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 N_PUNTOS = 100
 DESVIO_RUIDO = 1.5
 SEMILLA = 42
+W = 3
+B = 2
+
+
+def predecir(valores_x, w, b):
+    return w * valores_x + b 
+
+def perdida(valor_real, valor_esperado):
+    #error cuadratico medio
+    return np.average((valor_esperado - valor_real)**2)
 
 if __name__ == "__main__":
     #creamos el array inicial de valores para X
@@ -17,11 +27,15 @@ if __name__ == "__main__":
 
     valores_devio_y = valores_reales_y + array_ruido
 
+    resultados = predecir(array_x, W,B)
+
+    print(perdida(resultados, valores_devio_y))
+
     #plot - muestra los datos
     fig, ax = plt.subplots(figsize=(20, 6))
 
     ax.plot(array_x, valores_reales_y, 'o') 
     ax.plot(array_x, valores_devio_y, 'x')
-    
-    plt.show()
+
+    plt.show() 
     
